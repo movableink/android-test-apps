e1b6eb0052456de7b178ca90826bd60edcda8ae0
christine <christinewasabi@gmail.com>
2023-02-03 18:23:20 +0300

    app theme

--- app/src/main/java/com/movableink/app/ui/theme/Theme.kt
+++ app/src/main/java/com/movableink/app/ui/theme/Theme.kt
@@ -1,28 +1,27 @@
 package com.movableink.app.ui.theme
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.lightColorScheme
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.darkColors
+import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.TileMode
 
-private val DarkColorPalette = darkColorScheme(
+private val DarkColorPalette = darkColors(
     primary = Purple200,
+    primaryVariant = Purple700,
     secondary = Teal200
 )
 
-private val LightColorPalette = lightColorScheme(
+private val LightColorPalette = lightColors(
     primary = Purple500,
+    primaryVariant = Purple700,
     secondary = Teal200
 
-    /* Other default colors to override
-    background = Color.White,
-    surface = Color.White,
-    onPrimary = Color.White,
-    onSecondary = Color.Black,
-    onBackground = Color.Black,
-    onSurface = Color.Black,
-    */
 )
 
 @Composable
@@ -34,9 +33,21 @@ fun ShoppingCartTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Comp
     }
 
     MaterialTheme(
-        colorScheme = colors,
-        typography = Typography,
+        colors = colors,
+        typography = typography,
         shapes = Shapes,
         content = content
     )
 }
+fun Modifier.offsetGradientBackground(
+    colors: List<Color>,
+    width: Float,
+    offset: Float = 0f
+) = background(
+    Brush.horizontalGradient(
+        colors,
+        startX = -offset,
+        endX = width - offset,
+        tileMode = TileMode.Mirror
+    )
+)
--- app/src/main/java/com/movableink/app/ui/theme/Type.kt
+++ app/src/main/java/com/movableink/app/ui/theme/Type.kt
@@ -1,28 +1,70 @@
 package com.movableink.app.ui.theme
 
-import androidx.compose.material3.Typography
+import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.movableink.app.R
+
+private val fontFamily = FontFamily(
+    Font(R.font.oswald_bold),
+    Font(R.font.oswald_medium, FontWeight.W500),
+    Font(R.font.oswald_regular, FontWeight.Bold)
+)
 
 // Set of Material typography styles to start with
-val Typography = Typography(
-    bodyMedium = TextStyle(
-        fontFamily = FontFamily.Default,
+val typography = Typography(
+    h4 = TextStyle(
+        fontFamily = fontFamily,
+        fontWeight = FontWeight.W600,
+        fontSize = 30.sp
+    ),
+    h5 = TextStyle(
+        fontFamily = fontFamily,
+        fontWeight = FontWeight.W600,
+        fontSize = 24.sp
+    ),
+    h6 = TextStyle(
+        fontFamily = fontFamily,
+        fontWeight = FontWeight.W600,
+        fontSize = 20.sp
+    ),
+    subtitle1 = TextStyle(
+        fontFamily = fontFamily,
+        fontWeight = FontWeight.W500,
+        fontSize = 16.sp
+    ),
+    subtitle2 = TextStyle(
+        fontFamily = fontFamily,
+        fontWeight = FontWeight.W500,
+        fontSize = 14.sp
+    ),
+    body1 = TextStyle(
+        fontFamily = fontFamily,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp
-    )
-    /* Other default text styles to override
+    ),
+    body2 = TextStyle(
+        fontFamily = fontFamily,
+        fontSize = 14.sp,
+        lineHeight = 20.sp
+    ),
     button = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = fontFamily,
         fontWeight = FontWeight.W500,
         fontSize = 14.sp
     ),
     caption = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = fontFamily,
         fontWeight = FontWeight.Normal,
         fontSize = 12.sp
+    ),
+    overline = TextStyle(
+        fontFamily = fontFamily,
+        fontWeight = FontWeight.W500,
+        fontSize = 12.sp
+
     )
-    */
 )
