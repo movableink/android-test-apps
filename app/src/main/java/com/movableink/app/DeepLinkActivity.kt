9333c1e2b401b0c6d3e5e3edce162b13a4f34580
christine <christinewasabi@gmail.com>
2023-03-21 20:49:48 +0300

    add internal scheme

--- app/src/main/java/com/movableink/app/DeepLinkActivity.kt
+++ app/src/main/java/com/movableink/app/DeepLinkActivity.kt
@@ -3,13 +3,9 @@ package com.movableink.app
 import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import androidx.core.app.ComponentActivity
-import androidx.core.net.toUri
 import androidx.lifecycle.lifecycleScope
 import com.braze.ui.inappmessage.BrazeInAppMessageManager
-import com.movableink.app.ui.navigation.DeepLinkPattern
-import com.movableink.app.utils.URIPath
 import com.movableink.inked.MIClient
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -17,18 +13,9 @@ import kotlinx.coroutines.withContext
 
 @SuppressLint("RestrictedApi")
 class DeepLinkActivity : ComponentActivity() {
-    private val TAG = "DeepLinkActivity"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        /* // ATTENTION: This was auto-generated to handle app links.
-         val appLinkIntent: Intent = intent
-         val appLinkAction: String? = appLinkIntent.action
-         val appLinkData: Uri? = appLinkIntent.data
-         if (appLinkData != null) {
-             val uri = this.intent.data
-             fetchClickableLink(uri.toString())
-         }*/
         handleIntent(intent)
     }
     private fun handleIntent(intent: Intent?) {
@@ -62,33 +49,22 @@ class DeepLinkActivity : ComponentActivity() {
                  deepLinkToProductPage(it)
              }
          }*/
-        lifecycleScope.launch {
-            val resolvedLink = withContext(Dispatchers.IO) {
-                MIClient.resolveUrl(uri)
-            }
 
-            resolvedLink?.let {
-                deepLinkToProductPage(it)
+        when (urlScheme(uri)) {
+            Scheme.INTERNAL -> {
+                deepLinkToProductPage(uri, this@DeepLinkActivity)
             }
-        }
-    }
-
-    private fun deepLinkToProductPage(url: String) {
-        URIPath.getProductFromURI(url.toUri())?.let { productId ->
-            val productDetailIntent = Intent(
-                Intent.ACTION_VIEW,
-                "${DeepLinkPattern.baseDestination}/$productId".toUri(),
-                this@DeepLinkActivity,
-                MainActivity::class.java
-            ).apply {
-                flags = Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_SINGLE_TOP
+            Scheme.GLOBAL -> {
+                lifecycleScope.launch {
+                    val resolvedLink = withContext(Dispatchers.IO) {
+                        MIClient.resolveUrl(uri)
+                    }
+                    resolvedLink?.let {
+                        deepLinkToProductPage(it, this@DeepLinkActivity)
+                    }
+                }
             }
-
-            try {
-                startActivity(productDetailIntent)
-            } catch (e: Exception) {
-                Log.e(TAG, "Failed to send deep link pending intent", e)
-            }
+            Scheme.OTHER -> return
         }
     }
 }
