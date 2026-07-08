f72ab29228013bc1d8fa65f0fe85d04d9d035b61
Chayel J Heinsen <cheinsen@movableink.com>
2026-07-08 15:03:40 -0400

    Update Gradle

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
-    id("com.gradle.enterprise").version("3.8")
+    id("com.gradle.enterprise").version("3.13.1")
 //    id("de.fayard.refreshVersions") version "0.51.0"
+    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
 }
 
 gradleEnterprise {
